9a8ca8b4407d30fd427094536e0ea039b7336983
Jardim <gabrielrlj99@outlook.com>
2020-05-24 22:01:44 -0300

    criando post de cliente

--- src/main/java/com/jardim/controller/ClienteController.java
+++ src/main/java/com/jardim/controller/ClienteController.java
@@ -2,8 +2,11 @@ package com.jardim.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +21,16 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	
-	@GetMapping("/")
-	public String inicial(){
-		return "index";
-	}
-	
+
 	
 	@GetMapping
 	@ResponseBody
 	public List<Cliente> listaTodos(){
 		return this.clienteService.findAll();
+	}
+	
+	@PostMapping
+	public Cliente cadastrar(@RequestBody Cliente cliente) {
+		return this.clienteService.save(cliente);
 	}
 }
